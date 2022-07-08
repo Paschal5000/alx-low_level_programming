8f7222c0dc8f7d60f77541551fa4c81ee9d295b8
Nwankwo <paschalofficial22@gmail.com>
2022-07-07 17:08:49 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,28 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * * print_line -> printing line
- * * @n: integer params
+ * more_numbers - print 0 - 14 ten times and you
+ *		can only use _putchar three times
+ *
+ * Return: Always 0 (Success)
  */
-void print_line(int n)
-{
-	int x;
 
-	if (n <= 0)
-		_putchar('\n');
-	else
+void more_numbers(void)
+{
+	int num, row, count;
+
+	for (row = 1; row <= 10; ++row)
 	{
-		for (x = 0; x < n; x++)
-			_putchar(95);
+		for (count = 0; count <= 14; ++count)
+		{
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
+		}
 		_putchar('\n');
 	}
 }
