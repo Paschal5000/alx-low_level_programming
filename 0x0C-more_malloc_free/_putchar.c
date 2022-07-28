d0e2694aa32060980d7336e5753f40dc33393e8b
Nwankwo <paschalofficial22@gmail.com>
2022-07-28 08:08:06 -0700

    yy

--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -2,12 +2,12 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to print
+ * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, and the error is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-	return(writes(1, &c, 1));
+	return (write(1, &c, 1));
 }
